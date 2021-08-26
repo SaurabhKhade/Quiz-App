e09a7debb9d25f8e6460661ca79fce97033b9a87
Saurabh Khade <khadesaurabh056@gmail.com>
2021-08-26 16:19:31 +0530

    score will be shown on statistics page.

--- src/components/Quiz/statistics.js
+++ src/components/Quiz/statistics.js
@@ -3,7 +3,7 @@ import {useSelector,useDispatch} from 'react-redux';
 import {resetCounter,resetAnswers,stop,resetCurrent} from '../actions';
 
 export default function Stats({data}) {
-  const {answers} = useSelector(state=>state);
+  const {answers,counter} = useSelector(state=>state);
   let correct=0, incorrect=0, unattempt=0,total=data.length;
   const dispatch = useDispatch();
 
@@ -13,8 +13,14 @@ export default function Stats({data}) {
     else incorrect++;
   }
   
+  const score = correct*4 - incorrect;
+  
   let correctDeg = correct/total*360;
   let incorrectDeg = incorrect/total*360 + correctDeg;
+  
+  let timeTaken = data.length*30 - counter;
+  let min = Math.floor(timeTaken/60);
+  let sec = timeTaken%60;
   
   const style = {
     background: `
@@ -39,11 +45,14 @@ export default function Stats({data}) {
   return (
     <div className="statistics">
       <h1>Statistics</h1>
+      <p>Time Taken:- {min}:{sec}</p>
       <div className="summary">
-        <p className="success"><strong>Correct: {correct}</strong></p>
-        <p className="danger"><strong>Incorrect: {incorrect}</strong></p>
-        <p className="warn"><strong>Unattempted: {unattempt}</strong></p>
+        <p className="success">Correct: {correct}</p>
+        <p className="danger">Incorrect: {incorrect}</p>
+        <p className="warn">Unattempted: {unattempt}</p>
+        <p className="simple">Score: {score}</p>
       </div>
+      <h2>{score>=12?'Passed!!!':'Failed!'}</h2>
       <div className="pie-wrapper">
         <div className="pie" style={style}></div>
       </div>
